bdb52a84d9ed866577c90dce5645fd2818d0ebf4
charan <charanramagiri2004@gmail.com>
2025-03-07 19:45:27 +0530

    Assignment_1 completed

---
+++ JAVA-SCRIPT/Assignments/Assignment_1.js
@@ -0,0 +1,29 @@
+//1a) 
+alert('Good morning!');
+
+//1b)
+alert('Simon');
+
+//1c)
+10 + 5
+
+//1d)
+20 - 5
+
+//1e)
+2 + 2 - 5
+
+//1f)
+document.body.innerHTML = 'Good morning!';
+
+//1g)
+document.body.innerHTML = 'Simon';
+
+//1h)
+10 + 8 + 20
+
+//1i)
+100 - 50 - 20 + 200
+
+//1j)
+document.body.innerHTML = '';
