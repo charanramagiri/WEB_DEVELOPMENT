e2a15f9e01d8b6ed613205c43586aba1babdb52d
charan <charanramagiri2004@gmail.com>
2025-03-08 17:58:20 +0530

    Assignment names changed to exercises and added exercise-3

---
+++ JAVA-SCRIPT/Assignments/Exercise-3.js
@@ -0,0 +1,42 @@
+//3a)
+'My name is:'
+
+//3b)
+'Charan'
+
+//3c)
+'My name is:' + 'Charan'
+
+//3d)
+'Total Cost: $'+ (5+3)
+
+//3e)
+`Total Cost: $${5+3}`
+
+//3f)
+alert(`Total Cost: $${5+3}`);
+
+//3g)
+'Total Cost: $' + ((599+295)/100)
+
+//3h)
+`Total Cost: $${(599+295)/100}`
+
+//3i)
+alert(`Total Cost: $${(599+295)/100}`);
+
+//3j)
+alert(`Total Cost: $${(599+295)/100}
+Thank you, come again!`);
+
+//3k)
+`Items (${2 + 2}): $${(2 * 2095 + 2 * 799) / 100}`
+
+//3l)
+`Shipping & handling: $${(499 + 499) / 100}`
+
+//3m)
+`Total before tax: $${(2 * 2095 + 2 * 799 + 499 + 499) / 100}`
+
+//3n)
+`Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`
